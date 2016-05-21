39820e191eb881f28ebfd5aeeaaf858e329f293e
vlad ilie <vlad.ilie@unifiedpost.com>
2016-05-21 20:35:27 +0300

    More logging and error handle

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,13 +1,22 @@
 class ProxyController < ApplicationController
-  def index
+  rescue_from Exception do |exception|
+    Rails.logger.info(exception)
+    puts exception
+    data = MultiJson.load(open('test/index.json'))
     response.headers['Access-Control-Allow-Origin'] = '*'
-    render :json => (get_articles rescue MultiJson.load(open('test/index.json')))
+    render :json => data
+  end
+
+  def index
+    data = get_articles
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    render :json => data
   end
 
   def get_articles(query = nil)
     query ||= params.select{|k,_| k.to_sym == :q}
-    puts query
-    Rails.logger.info query
     Faraday.get("http://hacktm.ness.ro:8983/solr/article/select", {wt: :json, q: query})
+  rescue
+    MultiJson.load(open('test/index.json'))
   end
 end
